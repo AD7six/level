228a81e63ce4d3b32d5adc9f66cda8e33283708a
Andy Dawson <andydawson76@gmail.com>
2026-02-27 20:39:29 +0100

    Domain fixes to permit cleaner commands

--- src/level/domains/applications/__init__.py
+++ src/level/domains/applications/__init__.py
@@ -5,14 +5,16 @@ from .applications import (
     fix_applications,
     get_application,
     lint_applications,
+    list_application_rows,
     list_applications,
     move_application,
 )
-from .schema import STATES, TERMINAL_STATES
+from .schema import SORTED_STATES, STATES, TERMINAL_STATES
 
 __all__ = [
     "Application",
     "STATES",
+    "SORTED_STATES",
     "TERMINAL_STATES",
     "TRANSITIONS",
     "create_application",
@@ -20,5 +22,6 @@ __all__ = [
     "get_application",
     "lint_applications",
     "list_applications",
+    "list_application_rows",
     "move_application",
 ]
